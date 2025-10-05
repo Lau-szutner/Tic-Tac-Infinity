af05066a60a9cb0f3aa6a31b4d0c525d5393351d
Lautaro Fernandez Szutner <111034147+Lau-szutner@users.noreply.github.com>
2025-10-05 04:44:13 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Tic-tac-infinity/', 
+  base: '/Tic-Tac-Infinity/',
   plugins: [react(), tailwindcss()],
 });
